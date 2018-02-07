e916ca404dbdadf9e90886347157400ae2e4351e
George Cooper <georgecooper.dev@gmail.com>
2018-02-07 20:31:10 +0000

    Add files via upload

---
+++ GraphTraversal/GraphTraversal/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphTraversal
+{
+    class Node
+    {
+        // Properties
+        public Node left;
+        public Node right;
+        public string data;
+
+        // Constructor with inputs for child nodes
+        public Node(Node left, Node right, string data)
+        {
+            this.left = left;
+            this.right = right;
+            this.data = data;
+        }
+    }
+}
---
+++ GraphTraversal/GraphTraversal/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating the tree
+            Node tree = new Node(new Node(new Node(null,null,"D"),new Node(null,null,"E"),"B"), new Node(new Node(null,null,"F"), new Node(null,null,"G"), "C"), "A");
+
+            /*        
+             *   The equivalent Tree
+             *   -------------------
+             *            A
+             *          /   \
+             *         B     C
+             *        / \   / \
+             *       D   E F   G
+             *       
+             */
+
+            // Outputting Results to the depth first search
+            Console.WriteLine("Depth First Search:");
+            DFS(tree);
+            Console.Write("\n");
+
+            // Outputting Results for the Breadth First Search
+            Console.WriteLine("Breadth First Search:");
+            BFS(tree);
+
+            Console.ReadKey();
+        }
+        // Depth First Search Algorithm
+        static void DFS(Node tree)
+        {
+            if ((tree == null))
+            {
+                return;
+            }
+            Console.Write(tree.data + " ");
+            DFS(tree.left);
+            DFS(tree.right);
+        }
+
+        // Breath First Search Algorithm
+        static void BFS(Node tree)
+        {
+            Queue<Node> q = new Queue<Node>();
+            q.Enqueue(tree);
+
+            while(q.Count >0)
+            {
+                Node node = q.Dequeue();
+                if(node != null)
+                {
+                    Console.Write(node.data + " ");
+                    q.Enqueue(node.left);
+                    q.Enqueue(node.right);
+                }
+                
+            }
+        }
+    }
+}
